578cf40a670b74194687d0b2a9dab31c364d0266
Cassio Rodrigues <cassiobenner@gmail.com>
2021-05-23 19:24:06 -0300

    atulizacao

---
+++ core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def index(request):
+   context = {
+        'curso': 'curso de django freamwork',
+        'outro': 'outro curso'
+    }
+    return render(request, 'index.html', context)
+
+
+def contato(request):
+    return render(render, 'contato.html')
